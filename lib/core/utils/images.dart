73ab84ee6dd18623812e886c1031a69bd191ece8
A7medMg <asilme023@gmail.com>
2026-04-01 15:32:21 +0200

    change app name and icon . added assets folder with app icon image. added flutter launcher icon package .

---
+++ lib/core/utils/images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppIcon 
+  /// assets/images/app_icon .png
+  static const String assetsImagesAppIcon  = "assets/images/app_icon .png";
+
+  /// Assets for assetsImagesBottomSplash
+  /// assets/images/bottom_splash.svg
+  static const String assetsImagesBottomSplash = "assets/images/bottom_splash.svg";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.svg
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+
+  /// Assets for assetsImagesPlant
+  /// assets/images/plant.svg
+  static const String assetsImagesPlant = "assets/images/plant.svg";
+}
+
